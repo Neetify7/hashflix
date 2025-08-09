0c6b359077a6d5830ea5d26ae7e31548f0c17a22
Neetify7 <221894038+Neetify7@users.noreply.github.com>
2025-08-09 14:28:42 -0400

    Replaced Next.js boilerplate

---
+++ src/app/components/nav.tsx
@@ -0,0 +1,15 @@
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">HOME</a></li>
+                <li><a href="/">MOVIE</a></li>
+                <li><a href="/">TV</a></li>
+                <li><a href="/">ANIME</a></li>
+                <li><a href="/">SEARCH</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
